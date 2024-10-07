77a41401e6ea88c94ee54f0294a7a6f29422655e
Mavester432 <122857845+Mavester432@users.noreply.github.com>
2024-10-07 06:45:35 +0200

    0-pascal_triangle.py

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -7,14 +7,19 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
+    # Initialize the Pascal's triangle list
     triangle = [[1]]
 
     for i in range(1, n):
+        # Get the previous row
         prev_row = triangle[-1]
+        # Create the new row starting and ending with 1
         row = [1]
+        # Fill the middle values
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
         row.append(1)
+        # Add the new row to the triangle
         triangle.append(row)
 
     return triangle
